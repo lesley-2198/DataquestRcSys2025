cdbf11cedc0281bc558064aac6cbb9d366143b3a
Lesley Ngcobo <s225171406@mandela.ac.za>
2025-05-22 17:35:04 +0200

    Updated the comparison of the two collaboration filters

--- R_scripts/comparison_of_collab_filters.R
+++ R_scripts/comparison_of_collab_filters.R
@@ -7,13 +7,27 @@ ubcf_results <- evaluate(
   parameter = list(method = "Cosine", nn = 30)
 )
 
+# Combine evaluation results
+comparison_results <- evaluate(
+  x = eval_scheme,
+  method = list(
+    "User-Based CF" = list(name = "UBCF", param = list(method = "Cosine", nn = 30)),
+    "Item-Based CF" = list(name = "IBCF", param = list(method = "Cosine", k = 30))
+  ),
+  n = c(1, 3, 5, 10)
+)
 
-# Summarize UBCF
-ubcf_summary <- avg(ubcf_results)
+comparison_df <- data.frame(
+  Model = c("UBCF", "IBCF"),
+  Precision_at_5 = c(ubcf_summary["precision", "5"], ibcf_summary["precision", "5"]),
+  Recall_at_5 = c(ubcf_summary["recall", "5"], ibcf_summary["recall", "5"]),
+  TPR_at_5 = c(ubcf_summary["TPR", "5"], ibcf_summary["TPR", "5"]),
+  FPR_at_5 = c(ubcf_summary["FPR", "5"], ibcf_summary["FPR", "5"])
+)
 
-# Summarize IBCF
-ibcf_summary <- avg(ibcf_results)
 
-# Print them
-ubcf_summary
-ibcf_summary
+# Generate summary stats
+avg_comparison <- lapply(comparison_results, avg)
+avg_comparison
+
+
